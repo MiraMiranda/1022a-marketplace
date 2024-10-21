b81f878b35d683715fe9b6839fbf518577571e88
MiraMiranda <118369446+MiraMiranda@users.noreply.github.com>
2024-10-21 09:07:09 -0400

    arrumando

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,16 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
-
 app.get("/produtos", async(req,res)=>{
     //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
     //1 - Criar a conexão com o banco
     try{
         const conection = await mysql.createConnection({
-            host:process.env.dbhost? process.env.dbhost: "localhost",
-            user:process.env.dbuser? process.env.dbuser:"root",
-            password:process.env.dbpassword? process.env.dbpassword:"",
-            database:process.env.dbname? process.env.dbname:"banco1022a",
-            port:process.env.dbport?parseInt(process.env.dbport): 3306
+            host:process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         //2 - Realizar uma consulta na tabela
         const [result, fields] = await conection.query("SELECT * from produtos")
