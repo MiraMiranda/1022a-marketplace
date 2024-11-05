79458036ab77b42eb8d5a1115b24393308448e5e
MiraMiranda <118369446+MiraMiranda@users.noreply.github.com>
2024-11-05 10:25:56 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,8 @@ app.get("/produtos", async (req, res) => {
             database: process.env.dbname ? process.env.dbname : "banco1022a",
             port: process.env.dbport ? parseInt(process.env.dbport) : 3306
         })
-        const [result, fields] = await connection.query("SELECT * from produtos")
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result, fields] = await connection.query("INSERT INTO produtos VALUES(?,?,?,?,?),", [id,nome,descricao,preco,imagem])
         await connection.end()
         res.send(result)
     } catch (e) {
@@ -54,6 +55,7 @@ app.get("/usuarios", async (req, res) => {
         await connection.end()
         res.send(result)
     } catch (e) {
+        console.log(e)
         res.status(500).send("Server ERROR")
     }
 })
